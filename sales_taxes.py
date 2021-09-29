ca56e7ee04808fedb16ec81bcf1d4cd07e39e887
dimon <dimon@inqabeta.com>
2021-09-29 22:09:04 +0200

    [feat] finalize script

--- sales_taxes.py
+++ sales_taxes.py
@@ -1,11 +1,40 @@
 #!/usr/bin/env python3
 import re
+import sys
 from math import ceil
 
 from typing import Tuple
 
 TAX_FREE = ['book', 'chocolate', 'pill']
 LINE_RGX = re.compile(r"^(\d+) (.*) at (\d+\.\d{2})$")
+
+
+def main(argv):
+    if len(argv) != 2:
+        raise AttributeError("no basket file defined!")
+    with open(argv[1], "r", encoding="utf8") as f:
+        print(basket2receipt(f.readlines()))
+
+
+def basket2receipt(lines):
+    total = 0
+    taxes = 0
+    output = ""
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue  # empty line
+        amount, text, price = read_line(line)
+        if not price:
+            raise ValueError(f"abort program, malformed line:\n{line}")
+        tax_rate = get_tax_rate_by_description(text)
+        tax = round_up_tax(tax_rate * price * amount) if tax_rate else 0
+        total += price * amount + tax
+        taxes += tax
+        output += f"{amount} {text}: {(price * amount + tax):.2f}\n"
+    output += f"Sales Taxes: {taxes:.2f}\n"
+    output += f"Total: {total:.2f}"
+    return output
 
 
 def read_line(line: str) -> Tuple[int, str, float] or None:
@@ -29,3 +58,11 @@ def get_tax_rate_by_description(description: str):
 
 def round_up_tax(tax: float):
     return ceil(tax * 20) / 20
+
+
+if __name__ == '__main__':
+    try:
+        main(sys.argv)
+    except (AttributeError, ValueError) as err:
+        print(err)
+        exit(1)
--- test_sales_taxes.py
+++ test_sales_taxes.py
@@ -1,9 +1,34 @@
 import unittest
 
-from sales_taxes import round_up_tax, get_tax_rate_by_description, read_line
+from sales_taxes import round_up_tax, get_tax_rate_by_description, read_line, basket2receipt, main
 
 
 class MyTestCase(unittest.TestCase):
+    def test_basket2receipt(self):
+        input_1 = "1 book at 12.49\n1 music CD at 14.99\n1 chocolate bar at 0.85"
+        input_2 = " 1 imported box of chocolates at 10.00\n1 imported bottle of perfume at 47.50"
+        input_3 = "1 imported bottle of perfume at 27.99 \n1 bottle of perfume at 18.99 \n" \
+                  "1 packet of headache pills at 9.75 \n1 box of imported chocolates at 11.25"
+        input_4 = "1 imported bottle of perfume at 27.99 \n1 bottle of perfume at 18.99 \n" \
+                  "2 packet of headache pills at 9.75 \n1 box of imported chocolates at 11.25"  # like #3 but 2 packets
+        output_1 = "1 book: 12.49\n1 music CD: 16.49\n1 chocolate bar: 0.85\nSales Taxes: 1.50\nTotal: 29.83"
+        output_2 = "1 imported box of chocolates: 10.50\n1 imported bottle of perfume: 54.65\nSales Taxes: 7.65\n" \
+                   "Total: 65.15"
+        output_3 = "1 imported bottle of perfume: 32.19\n1 bottle of perfume: 20.89\n" \
+                   "1 packet of headache pills: 9.75\n1 box of imported chocolates: 11.85\nSales Taxes: 6.70\n" \
+                   "Total: 74.68"
+        output_4 = "1 imported bottle of perfume: 32.19\n1 bottle of perfume: 20.89\n" \
+                   "2 packet of headache pills: 19.50\n1 box of imported chocolates: 11.85\nSales Taxes: 6.70\n" \
+                   "Total: 84.43"
+        self.assertEqual(output_1, basket2receipt(input_1.splitlines()))
+        self.assertEqual(output_2, basket2receipt(input_2.splitlines()))
+        self.assertEqual(output_3, basket2receipt(input_3.splitlines()))
+        self.assertEqual(output_4, basket2receipt(input_4.splitlines()))
+
+    def test_main(self):
+        self.assertRaises(AttributeError, main, ["one"])  # must have at least 2 args
+        self.assertRaises(ValueError, main, ["me", "fail_test.txt"])
+
     def test_read_line(self):
         self.assertTupleEqual((1, "imported box of chocolates", 10.00),
                               read_line("1 imported box of chocolates at 10.00"))
