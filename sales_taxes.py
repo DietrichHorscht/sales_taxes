2b7a0767decf873d6234cc047e19547acd0f131c
dimon <dimon@inqabeta.com>
2021-09-29 22:00:40 +0200

    [feat] get tax rate

--- sales_taxes.py
+++ sales_taxes.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 from math import ceil
 
+TAX_FREE = ['book', 'chocolate', 'pill']
+
+
+def get_tax_rate_by_description(description: str):
+    has_tax = True
+    has_duty = False
+    for word in description.split():
+        if [free for free in TAX_FREE if word.upper().startswith(free.upper())]:
+            has_tax = False
+            continue
+        if "imported" == word.lower():
+            has_duty = True
+    return round((0.1 if has_tax else 0.0) + (0.05 if has_duty else 0.0), 2)
+
+
 def round_up_tax(tax: float):
     return ceil(tax * 20) / 20
--- test_sales_taxes.py
+++ test_sales_taxes.py
@@ -1,9 +1,23 @@
 import unittest
 
-from sales_taxes import round_up_tax
+from sales_taxes import round_up_tax, get_tax_rate_by_description
 
 
 class MyTestCase(unittest.TestCase):
+    def test_get_tax_rate(self):
+        # books
+        self.assertEqual(0.0, get_tax_rate_by_description("Harry Potter BOOK"))
+        self.assertEqual(0.0, get_tax_rate_by_description("Harry Potter books"))
+        # food
+        self.assertEqual(0.0, get_tax_rate_by_description("lindt chocolates white"))
+        # medicine
+        self.assertEqual(0.0, get_tax_rate_by_description("aspirin pills"))
+        # others
+        self.assertEqual(0.1, get_tax_rate_by_description("blue car"))
+        # duty
+        self.assertEqual(0.15, get_tax_rate_by_description("imported blue car"))
+        self.assertEqual(0.05, get_tax_rate_by_description("imported chocolate"))
+
     def test_round_up_tax(self):
         self.assertEqual(0.05, round_up_tax(0.02))
         self.assertEqual(0.05, round_up_tax(0.05))
